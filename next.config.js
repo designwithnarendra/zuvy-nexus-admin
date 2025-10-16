72a46586755772d9eaf395f45c1653536a8d3e3e
Narendra SIngh <narendra0014@gmail.com>
2025-10-16 20:13:31 +0530

    Update Next.js config for export and image settings

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['placeholder.svg'],
   },
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/zuvy-nexus-admin>',
+  assetPrefix: '/zuvy-nexus-admin/',
 }
 
 module.exports = nextConfig
